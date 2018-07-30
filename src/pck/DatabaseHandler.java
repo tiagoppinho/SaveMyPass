c5d0ee1572a6fae56a01f25f70fc3bf003a43864
Tiago Pinho <tiagoppinho@gmail.com>
2018-07-30 21:37:54 +0100

    Added initial database handler

---
+++ src/pck/DatabaseHandler.java
@@ -0,0 +1,24 @@
+package pck;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Tiago Pinho
+ */
+public class DatabaseHandler {
+    
+    private static final String CONNECTION_PATH = "jdbc:sqlite:db/passwordmanager.sqlite";
+    
+    public static Connection getConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(CONNECTION_PATH);
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
