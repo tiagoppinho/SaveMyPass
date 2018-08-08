6853311c3db7cd6a33d535ae5c169e5361adbf56
Tiago Pinho <tiagoppinho@gmail.com>
2018-08-08 21:43:52 +0100

    Simplified Database Handler

--- src/pck/DatabaseHandler.java
+++ src/pck/DatabaseHandler.java
@@ -2,10 +2,7 @@ package pck;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
@@ -27,46 +24,5 @@ public class DatabaseHandler {
             ex.printStackTrace();
         }
         return null;
-    }
-    
-    /**
-     * Gets the master pin on database and returns it as a string.
-     * @return String
-     */
-    public static String getDatabaseCurrentPin(){
-        Connection connection = getConnection();
-        String databaseCurrentPin = "";
-        try{
-            connection.setAutoCommit(false);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT pin FROM User");
-            resultSet.next();
-            databaseCurrentPin = resultSet.getString("pin");
-            resultSet.close();
-            statement.close();
-            connection.close();
-        }catch(SQLException ex){
-            ex.printStackTrace();
-        }
-        return databaseCurrentPin;
-    }
-    
-    /**
-     * Updates the master pin and salt on database.
-     * @param pin New master pin.
-     * @param salt New salt.
-     */
-    public static void updatePinOnDatabase(String pin, String salt){
-        Connection connection = DatabaseHandler.getConnection();
-        try {
-            connection.setAutoCommit(false);
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("UPDATE User SET pin = " + pin + ", salt = " + salt);
-            connection.commit();
-            statement.close();
-            connection.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 }
