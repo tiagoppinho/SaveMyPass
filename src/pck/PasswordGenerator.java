2eef6c6b37950f39f9cb56a483d51e733eade912
Tiago Pinho <tiagoppinho@gmail.com>
2018-08-12 01:07:29 +0100

    Removed main from PasswordGenerator

--- src/pck/PasswordGenerator.java
+++ src/pck/PasswordGenerator.java
@@ -87,8 +87,4 @@ public class PasswordGenerator {
         
         return sb.toString();
     }
-    
-    public static void main(String[] args) {
-        System.out.println(new PasswordGenerator().generateRandomSecurePassword());
-    }
 }
