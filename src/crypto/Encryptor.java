aa082953525dce442c974b6a16fb5d1138521c2d
Tiago Pinho <tiagoppinho@gmail.com>
2018-08-23 19:04:29 +0100

    Fixed Encryptor

--- src/crypto/Encryptor.java
+++ src/crypto/Encryptor.java
@@ -1,15 +1,18 @@
 package crypto;
 
 import handlers.DatabaseHandler;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -22,20 +25,18 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
- * Class responsible for encryption/decryption using AES-256.
+ * Class responsible for encryption/decryption using AES-128.
  * @author Tiago Pinho
  */
 public class Encryptor {
     
-    private String keyLocker, salt;
-    
-    private IvParameterSpec initializationVector;
-    
+    private String locker, salt;
+        
     private final SecretKeySpec key;
         
     private final String KEY_ALGORITHM = "AES",
                          ENCRYPTION_ALGORITHM = "AES/CBC/PKCS5Padding",
-                         SECRET_KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA512";
+                         SECRET_KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA256";
     
     private final int ITERATIONS = 10000, KEY_SIZE = 128;    
     
@@ -45,49 +46,58 @@ public class Encryptor {
     }
     
     /**
-     * Encrypts a string using AES-256.
+     * Encrypts a string using AES-128.
      * @param string String to encrypt.
-     * @return String
+     * @return String - encrypted string.
      */
     public String encrypt(String string){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try{
             Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] iv = cipher.getParameters().getParameterSpec(IvParameterSpec.class).getIV();
             byte[] encryptedString = cipher.doFinal(string.getBytes());
-            return Base64.getEncoder().encodeToString(encryptedString);
-        }catch(InvalidKeyException | IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException ex){
+            outputStream.write(iv);
+            outputStream.write(encryptedString);
+            
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        }catch(IOException | InvalidKeyException | InvalidParameterSpecException | IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException ex){
             System.out.println(ex.toString());
         }
         return null;
     }
     
     /**
-     * Decrypts an AES-256 encrypted string.
+     * Decrypts an AES-128 encrypted string.
      * @param encryptedString Encrypted string to decrypt.
-     * @return String
+     * @return String - decrypted string.
      */
     public String decrypt(String encryptedString){
         try{
+            byte[] decodedString = Base64.getDecoder().decode(encryptedString),
+                   iv = Arrays.copyOfRange(decodedString, 0, 16),
+                   encrypted = Arrays.copyOfRange(decodedString, 16, decodedString.length);
+            
             Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key, initializationVector);
-            byte[] decryptedString = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
-            return Base64.getEncoder().encodeToString(decryptedString);
-        }catch(InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException ex){
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+            
+            return new String(cipher.doFinal(encrypted));
+        }catch(InvalidKeyException | IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException ex){
             System.out.println(ex.toString());
         }
         return null;
     }
     
     /**
-     * Generates the SecretKeySpec based on key locker and salt.
-     * @return SecretKeySpec
+     * Generates the SecretKeySpec based on locker and salt.
+     * @return SecretKeySpec - Generated key.
      */
     private SecretKeySpec generateKey(){
         try{
-            SecretKeyFactory skf = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_ALGORITHM);
-            PBEKeySpec spec = new PBEKeySpec(keyLocker.toCharArray(), salt.getBytes(),
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_ALGORITHM);
+            PBEKeySpec keySpec = new PBEKeySpec(locker.toCharArray(), salt.getBytes(),
                                              ITERATIONS, KEY_SIZE);
-            SecretKey secretKey = skf.generateSecret(spec);
+            SecretKey secretKey = keyFactory.generateSecret(keySpec);
             return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
         }catch(NoSuchAlgorithmException | InvalidKeySpecException ex){
             System.out.println(ex.toString());
@@ -104,14 +114,13 @@ public class Encryptor {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Locker");
             resultSet.next();
-            this.keyLocker = resultSet.getString("locker");
+            this.locker = resultSet.getString("locker");
             this.salt = resultSet.getString("salt");
-            this.initializationVector = new IvParameterSpec(Base64.getDecoder().decode(resultSet.getString("iv")));
             resultSet.close();
             statement.close();
             connection.close();
         }catch(SQLException ex){
-            ex.printStackTrace();
+            System.out.println(ex.toString());
         }
     }
 }
