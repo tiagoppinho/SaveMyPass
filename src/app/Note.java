450e6350b7d718dea0c38350dde768a241f57396
Tiago Pinho <tiagoppinho@gmail.com>
2018-09-14 00:55:29 +0100

    Improved Note UI

--- src/app/Note.java
+++ src/app/Note.java
@@ -40,6 +40,7 @@ public class Note extends javax.swing.JFrame {
 
         headerPanel = new javax.swing.JPanel();
         title = new javax.swing.JLabel();
+        btnCancel = new javax.swing.JLabel();
         mainPanel = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         txtNoteTitle = new javax.swing.JTextField();
@@ -47,32 +48,39 @@ public class Note extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         txtNoteDescription = new javax.swing.JTextArea();
         footerPanel = new javax.swing.JPanel();
-        btnCancel = new javax.swing.JLabel();
-        btnAdd = new javax.swing.JLabel();
+        btnAddOrSave = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-        setTitle("SaveMyPass - Note");
+        setTitle("SaveMyPass - New Note");
         setMinimumSize(new java.awt.Dimension(379, 348));
         setUndecorated(true);
         setResizable(false);
 
         headerPanel.setBackground(new java.awt.Color(0, 39, 255));
+        headerPanel.setLayout(null);
 
         title.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         title.setForeground(new java.awt.Color(255, 255, 255));
         title.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        title.setText("New Card");
+        title.setText("New Note");
+        headerPanel.add(title);
+        title.setBounds(50, 0, 270, 50);
 
-        javax.swing.GroupLayout headerPanelLayout = new javax.swing.GroupLayout(headerPanel);
-        headerPanel.setLayout(headerPanelLayout);
-        headerPanelLayout.setHorizontalGroup(
-            headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(title, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
-        );
-        headerPanelLayout.setVerticalGroup(
-            headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(title, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
-        );
+        btnCancel.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
+        btnCancel.setForeground(new java.awt.Color(255, 255, 255));
+        btnCancel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btnCancel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/app/img/Cancel_24px_white.png"))); // NOI18N
+        btnCancel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCancel.setMaximumSize(new java.awt.Dimension(61, 37));
+        btnCancel.setMinimumSize(new java.awt.Dimension(61, 37));
+        btnCancel.setPreferredSize(new java.awt.Dimension(61, 37));
+        btnCancel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnCancelMousePressed(evt);
+            }
+        });
+        headerPanel.add(btnCancel);
+        btnCancel.setBounds(340, 10, 20, 20);
 
         mainPanel.setBackground(new java.awt.Color(250, 250, 250));
         mainPanel.setName(""); // NOI18N
@@ -118,53 +126,32 @@ public class Note extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(28, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         footerPanel.setBackground(new java.awt.Color(250, 250, 250));
+        footerPanel.setMinimumSize(new java.awt.Dimension(380, 70));
 
-        btnCancel.setBackground(new java.awt.Color(153, 153, 153));
-        btnCancel.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
-        btnCancel.setForeground(new java.awt.Color(255, 255, 255));
-        btnCancel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        btnCancel.setText("Cancel");
-        btnCancel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnCancel.setMaximumSize(new java.awt.Dimension(61, 37));
-        btnCancel.setMinimumSize(new java.awt.Dimension(61, 37));
-        btnCancel.setOpaque(true);
-        btnCancel.setPreferredSize(new java.awt.Dimension(61, 37));
-        btnCancel.addMouseListener(new java.awt.event.MouseAdapter() {
+        btnAddOrSave.setBackground(new java.awt.Color(51, 153, 255));
+        btnAddOrSave.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
+        btnAddOrSave.setForeground(new java.awt.Color(255, 255, 255));
+        btnAddOrSave.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btnAddOrSave.setText("Add");
+        btnAddOrSave.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAddOrSave.setMaximumSize(new java.awt.Dimension(61, 37));
+        btnAddOrSave.setMinimumSize(new java.awt.Dimension(61, 37));
+        btnAddOrSave.setOpaque(true);
+        btnAddOrSave.setPreferredSize(new java.awt.Dimension(61, 37));
+        btnAddOrSave.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnCancelMouseEntered(evt);
+                btnAddOrSaveMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnCancelMouseExited(evt);
+                btnAddOrSaveMouseExited(evt);
             }
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                btnCancelMousePressed(evt);
-            }
-        });
-
-        btnAdd.setBackground(new java.awt.Color(51, 153, 255));
-        btnAdd.setFont(new java.awt.Font("Arial", 1, 13)); // NOI18N
-        btnAdd.setForeground(new java.awt.Color(255, 255, 255));
-        btnAdd.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        btnAdd.setText("Add");
-        btnAdd.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnAdd.setMaximumSize(new java.awt.Dimension(61, 37));
-        btnAdd.setMinimumSize(new java.awt.Dimension(61, 37));
-        btnAdd.setOpaque(true);
-        btnAdd.setPreferredSize(new java.awt.Dimension(61, 37));
-        btnAdd.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnAddMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnAddMouseExited(evt);
-            }
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                btnAddMousePressed(evt);
+                btnAddOrSaveMousePressed(evt);
             }
         });
 
@@ -173,19 +160,15 @@ public class Note extends javax.swing.JFrame {
         footerPanelLayout.setHorizontalGroup(
             footerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(footerPanelLayout.createSequentialGroup()
-                .addGap(59, 59, 59)
-                .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
-                .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(64, 64, 64))
+                .addGap(133, 133, 133)
+                .addComponent(btnAddOrSave, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(139, Short.MAX_VALUE))
         );
         footerPanelLayout.setVerticalGroup(
             footerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(footerPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(footerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnAddOrSave, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(28, Short.MAX_VALUE))
         );
 
@@ -193,58 +176,50 @@ public class Note extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(headerPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(footerPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(headerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(footerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 350, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(headerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 230, Short.MAX_VALUE)
+                .addComponent(footerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(headerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, 0)
+                    .addGap(0, 50, Short.MAX_VALUE)
                     .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, 0)
-                    .addComponent(footerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                    .addGap(0, 70, Short.MAX_VALUE)))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnCancelMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelMouseEntered
-        btnCancel.setBackground(Constants.CANCEL_BUTTONS_COLOR_ON_MOUSE_HOVER);
-    }//GEN-LAST:event_btnCancelMouseEntered
-
-    private void btnCancelMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelMouseExited
-        btnCancel.setBackground(Constants.CANCEL_BUTTONS_DEFAULT_COLOR);
-    }//GEN-LAST:event_btnCancelMouseExited
-
     private void btnCancelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCancelMousePressed
         close();
     }//GEN-LAST:event_btnCancelMousePressed
 
-    private void btnAddMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMouseEntered
-        btnAdd.setBackground(Constants.BUTTONS_COLOR_ON_MOUSE_HOVER);
-    }//GEN-LAST:event_btnAddMouseEntered
+    private void btnAddOrSaveMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddOrSaveMouseEntered
+        btnAddOrSave.setBackground(Constants.BUTTONS_COLOR_ON_MOUSE_HOVER);
+    }//GEN-LAST:event_btnAddOrSaveMouseEntered
 
-    private void btnAddMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMouseExited
-        btnAdd.setBackground(Constants.BUTTONS_DEFAULT_COLOR);
-    }//GEN-LAST:event_btnAddMouseExited
+    private void btnAddOrSaveMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddOrSaveMouseExited
+        btnAddOrSave.setBackground(Constants.BUTTONS_DEFAULT_COLOR);
+    }//GEN-LAST:event_btnAddOrSaveMouseExited
 
-    private void btnAddMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddMousePressed
+    private void btnAddOrSaveMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddOrSaveMousePressed
         if(index == 0){
             String noteTitle = txtNoteTitle.getText(), noteDescription = txtNoteDescription.getText();
-            
+
             if(noteTitle.isEmpty() || noteDescription.isEmpty()){
                 Customization.displayWarningMessage(
                     "Please fill all the card fields.",
@@ -273,7 +248,7 @@ public class Note extends javax.swing.JFrame {
                 close();
             }
         }
-    }//GEN-LAST:event_btnAddMousePressed
+    }//GEN-LAST:event_btnAddOrSaveMousePressed
 
     /**
      * Closes this frame and enables dashboard.
@@ -314,7 +289,7 @@ public class Note extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel btnAdd;
+    private javax.swing.JLabel btnAddOrSave;
     private javax.swing.JLabel btnCancel;
     private javax.swing.JPanel footerPanel;
     private javax.swing.JPanel headerPanel;
